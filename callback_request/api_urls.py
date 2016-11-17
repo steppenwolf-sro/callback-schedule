8041c565cb9999bb1959641402f19a0b33adcbf4
ookami-kb <ookami.kb@gmail.com>
2016-11-18 01:09:45 +0500

    added availability api

--- callback_request/api_urls.py
+++ callback_request/api_urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from callback_request.api_views import CreateCallbackRequest
+from callback_request.api_views import CreateCallbackRequest, ManagersAvailabilityView
 
 urlpatterns = [
     url(r'^create\.json$', CreateCallbackRequest.as_view()),
+    url(r'^availability\.json$', ManagersAvailabilityView.as_view()),
 ]
