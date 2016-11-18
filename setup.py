8695002a0bf6e05f25a661647928d57a488ac570
ookami-kb <ookami.kb@gmail.com>
2016-11-18 23:43:34 +0500

    updated setup

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(
-    name='django-callback',
-    packages=['django-callback'],
+    name='callback_schedule',
+    packages=['callback_schedule'],
     version='0.1',
     description='REST API for callback service for Django',
     author='SWDT',
