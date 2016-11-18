c2304eb965652f88ada5deead243bcf07a2b8dd2
ookami-kb <ookami.kb@gmail.com>
2016-11-19 00:29:31 +0500

    fixed protected permission

--- django_callback_app/utils.py
+++ django_callback_app/utils.py
@@ -5,3 +5,8 @@ class ProtectedPermission(permissions.DjangoModelPermissions):
     def __init__(self):
         super().__init__()
         self.perms_map['GET'] = ['%(app_label)s.change_%(model_name)s']
+
+    def has_permission(self, request, view):
+        if request.method == 'OPTIONS':
+            return True
+        return super().has_permission(request, view)
