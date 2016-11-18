98e0f13cce60589e1f11c7c89a63f256c6e86d68
ookami-kb <ookami.kb@gmail.com>
2016-11-18 23:49:14 +0500

    updated setup

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
-from distutils.core import setup
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='callback_schedule',
-    packages=['callback_schedule'],
+    name='django-callback',
     version='0.1',
-    description='REST API for callback service for Django',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',
+    description='Django app for managing callback requests',
+    long_description=README,
+    url='https://github.com/steppenwolf-sro/callback-schedule',
     author='SWDT',
     author_email='support@swdt.cz',
-    url='https://github.com/steppenwolf-sro/callback-schedule',
-    keywords=[],
-    classifiers=[],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
 )
