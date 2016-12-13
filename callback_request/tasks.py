89946d9088aa080221f43518918011e75661e9cd
ookami-kb <ookami.kb@gmail.com>
2016-12-13 15:59:42 +0500

    added task for delayed requests

--- callback_request/tasks.py
+++ callback_request/tasks.py
@@ -1,10 +1,10 @@
 from celery.task import task
 
-from callback_request.models import CallbackRequest
-
 
 @task(name='callback_request.delayed_request')
 def delayed_request(request_id):
+    from callback_request.models import CallbackRequest
+
     callback_request = CallbackRequest.objects.get(pk=request_id)
     if callback_request.completed or callback_request.error:
         return
